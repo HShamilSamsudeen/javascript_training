f6996595799584100fdea2dd0f401bd1c8332c26
Shamil Samsudeen <73551708+HShamilSamsudeen@users.noreply.github.com>
2022-05-13 18:02:38 -0400

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -223,44 +223,127 @@ Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 tall.
 */
 
-// Q1. & Q2.
-const Mark = {
-  firstName: "Mark",
-  lastName: "Miller",
-  fullName: function () {
-    return this.firstName + " " + this.lastName;
-  },
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    return (this.mass / this.height ** 2).toFixed(1);
-  },
-};
 
-const John = {
-  firstName: "John",
-  lastName: "Smith",
-  fullName: function () {
-    return this.firstName + " " + this.lastName;
-  },
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = (this.mass / this.height ** 2).toFixed(1);
-    return this.bmi;
-  },
-};
+// Q1. & Q2.
+// const Mark = {
+//   firstName: "Mark",
+//   lastName: "Miller",
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     return (this.mass / this.height ** 2).toFixed(1);
+//   },
+// };
+
+// const John = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.bmi = (this.mass / this.height ** 2).toFixed(1);
+//     return this.bmi;
+//   },
+// };
 
 // Q3.
-if (John.calcBMI() > Mark.calcBMI()) {
-  console.log(
-    `John's BMI (${John.calcBMI()}) is higher than Mark's (${Mark.calcBMI()})!"`
-  );
-} else if (John.calcBMI() < Mark.calcBMI()) {
-  console.log(
-    `John's BMI (${John.calcBMI()}) is lower than Mark's (${Mark.calcBMI()})!"`
-  );
-} else {
-  console.log(`They both have equal BMI's!`);
+// if (John.calcBMI() > Mark.calcBMI()) {
+//   console.log(
+//     `John's BMI (${John.calcBMI()}) is higher than Mark's (${Mark.calcBMI()})!"`
+//   );
+// } else if (John.calcBMI() < Mark.calcBMI()) {
+//   console.log(
+//     `John's BMI (${John.calcBMI()}) is lower than Mark's (${Mark.calcBMI()})!"`
+//   );
+// } else {
+//   console.log(`They both have equal BMI's!`);
+// }
+
+
+
+
+/* 
+Coding Challenge #4
+
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays 😉
+Bonus:
+
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to
+solve it:
+
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+
+4.3. Call the function with the 'totals' array 
+*/
+
+// Q1.
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+// Q2.
+const tips = new Array();
+const totals = new Array();
+
+// Q3.
+const calcTip = billValue => {
+  if (billValue >= 50 || billValue <= 300) {
+    return parseFloat(billValue * 0.15);
+  } else {
+    return parseFloat(billValue * 0.2);
+  }
+};
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(tips[i] + bills[i]);
 }
 
+console.log(`bills array: ${bills}`);
+console.log(`tips array: ${tips}`);
+console.log(`totals array: ${totals}`);
+
+//  Q4.
+function calcAverage(arr) {
+  let flag = false;
+  array_check = Array.isArray(arr);
+  array_check ? (flag = true) : console.log("Input an array..");
+
+  while (flag) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    average = total / arr.length;
+    return average;
+  }
+  return -1;
+}
+
+console.log(`\nExecuting calcAverage.. `);
+console.log(calcAverage(bills));
+console.log(`Done `);
